193e31b1afb7fbac51ae00d05b691d0173c62862
Eugen270181 <sadeugen@gmail.com>
2023-10-27 15:55:48 +0300

    feat:feat but not all

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const openBrackets = [];
+  const closeBrackets = [];
+  let pairBrackets = {};
+  for(i=0;i<bracketsConfig.length;i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+    closeBrackets.push(bracketsConfig[i][1]);
+    pairBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+
+  for(i=0;i<str.length;i++) {
+
+  }
+
 }
