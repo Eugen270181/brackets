edd1aa8f09a614e6c4676daf37dd02479af64ab4
Eugen Sadovnikov <sadeugen@gmail.com>
2023-10-27 22:14:55 +0300

    feat: feat all

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const stack = [];
   const openBrackets = [];
   const closeBrackets = [];
-  let pairBrackets = {};
+  const pairBrackets = {};
+  
   for(i=0;i<bracketsConfig.length;i++) {
     openBrackets.push(bracketsConfig[i][0]);
     closeBrackets.push(bracketsConfig[i][1]);
     pairBrackets[bracketsConfig[i][1]] = bracketsConfig[i][0];
   }
 
-  for(i=0;i<str.length;i++) {
-
-  }
-
+  return str
+         .split('')
+         .every( (el, index) => { 
+           if (openBrackets.some((e)=>e===el)) {
+             if (!((closeBrackets.some((e)=>e===el))&&(stack.length>0)&&(stack[stack.length-1]===el))) stack.push(el);
+             else stack.pop();
+           }
+           else {
+             if (stack.length===0) return false; 
+             if (stack.pop()!==pairBrackets[el]) return false;
+           }; 
+           return ( (index===str.length-1)&&(!(stack.length===0)) )?false:true;
+         });
 }
